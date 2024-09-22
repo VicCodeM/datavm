6d6825601e230e8a24ca7187f15c2fb9ed39f081
Victor Maldonado <vicomatters@live.com>
2024-09-21 20:51:03 -0600

    fix

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
+
+
 from setuptools import setup, find_packages
 
 setup(
     name='datavm',
-    version='0.1',
-    description='Una librería para análisis y manipulación de datos',
-    author='Víctor Maldoando',
-    author_email='vico.luis.ads@gmail.com',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'pandas',
-        'numpy',
-        'matplotlib',
-        'seaborn',
-        'scikit-learn'
+       # Agrega otras dependencias aquí si es necesario
     ],
+    author='Víctor M.',
+    author_email='vico.luis.ads@gmail.com',
+    description='Una librería para análisis y manipulación de datos',
+    url='https://github.com/VicCodeM/datavm',  # URL del repositorio
 )
